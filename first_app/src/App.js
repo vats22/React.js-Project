a5784f17d9bb8bc5c0cead6bbb8375d4634ce360
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2020-07-27 16:18:23 +0530

    part-1

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
+import Menu from './Components/MenuComponent';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-      <Navbar dark color='primary'>
-      <div className='container'>
-        <NavbarBrand href='/'>Ristornet con fusion</NavbarBrand>
-      </div>
-      </Navbar>
+      <div >
+        <Navbar dark color='primary'>
+          <div className='container'>
+            <NavbarBrand href='/'>Ristornet con fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
 
 
       </div>
