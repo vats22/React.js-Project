f8c059b3c5ca8afcabd8a47ff7c5f754a771e818
vatsshroff22@gmail.com <vatsshroff22@gmail.com>
2020-07-29 19:26:47 +0530

    part-2

--- first_app/src/App.js
+++ first_app/src/App.js
@@ -2,9 +2,19 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar,NavbarBrand} from 'reactstrap';
 import Menu from './Components/MenuComponent';
+import { DISHES } from './Shred/dishes';
 import './App.css';
 
 class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state={
+      dishes:DISHES
+    }
+  }
+
   render() {
     return (
       <div >
@@ -13,7 +23,7 @@ class App extends Component {
             <NavbarBrand href='/'>Ristornet con fusion</NavbarBrand>
           </div>
         </Navbar>
-        <Menu />
+        <Menu dishes={this.state.dishes} />
 
 
       </div>
